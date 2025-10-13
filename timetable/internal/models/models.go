7f6dcee8b4ab6c8ccf6933d26d4488c6bfa5eaa9
joduchosal <joduchosal@a208pc26l.local.isima.fr>
2025-10-13 10:03:15 +0200

    feat(events): add events CRUD - in progress

--- timetable/internal/models/models.go
+++ timetable/internal/models/models.go
@@ -2,9 +2,27 @@ package models
 
 import (
 	"github.com/gofrs/uuid"
+	"time"
 )
 
 type User struct {
 	Id   *uuid.UUID `json:"id"`
 	Name string     `json:"name"`
+}
+
+type Event struct {
+	Id          *uuid.UUID  `json:"id"`
+	AgendaIds   []uuid.UUID `json:"agendaIds"`
+	Uid         string      `json:"uid"`
+	Description string      `json:"description"`
+	Name        string      `json:"name"`
+	Start       time.Time   `json:"start"`
+	End         time.Time   `json:"end"`
+	Location    string      `json:"location"`
+	LastUpdate  time.Time   `json:"lastUpdate"`
+}
+
+type EventAgenda struct {
+	EventId  uuid.UUID `json:"eventId"`
+	AgendaId uuid.UUID `json:"agendaId"`
 }
