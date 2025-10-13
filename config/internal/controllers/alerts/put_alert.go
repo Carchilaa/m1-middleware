67b464b7b5414b05c066ad3ccd521600d57f202f
Chloe <chloedespesse0@gmail.com>
2025-10-13 10:07:52 +0200

    refactor(alerts)

---
+++ config/internal/controllers/alerts/put_alert.go
@@ -0,0 +1,50 @@
+package alerts
+
+import (
+	"github.com/gofrs/uuid"
+	"encoding/json"
+	"middleware/example/internal/helpers"
+	"middleware/example/internal/services/alerts"
+	"net/http"
+)
+
+// PutAlert
+// @Tags         alerts
+// @Summary      Create new alert.
+// @Description  Create new alert.
+// @Success      200            {array}  models.Alerts
+// @Failure      500             "Something went wrong while creating alert {id}"
+// @Router       /alerts [put]
+type PutAlertRequest struct{
+	Email string `json:"email"`
+	AgendaId uuid.UUID `json:"agendaId"`
+}
+
+
+func PutAlert(w http.ResponseWriter, r *http.Request) {
+	var req PutAlertRequest
+
+	//Vérifier que le body n'est pas nul
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	//Génère nouvel id
+	id := uuid.Must(uuid.NewV4())
+
+	alerts, err := alerts.PutAlert(id, req.Email, req.AgendaId)
+	if err != nil {
+		body, status := helpers.RespondError(err)
+		w.WriteHeader(status)
+		if body != nil {
+			_, _ = w.Write(body)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	body, _ := json.Marshal(alerts)
+	_, _ = w.Write(body)
+	return
+}
